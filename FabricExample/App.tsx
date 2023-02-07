a94425601e5a5f4e167a30ca4e54e31309102e03
Jakub Piasecki <jakub.piasecki@swmansion.com>
2023-02-07 10:57:42 +0100

    Make FabricExample match example app

--- FabricExample/App.tsx
+++ FabricExample/App.tsx
@@ -1,61 +1,127 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
+ * Copyright (c) Facebook, Inc. and its affiliates.
  *
- * @format
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
  */
 
-import React from 'react';
-import {TextInput, View, Button} from 'react-native';
+import React, {useCallback, useMemo, useState} from 'react';
+import {
+  Button,
+  Keyboard,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import Basic from './examples/Basic';
+import Functional from './examples/Functional';
+import GetSetClear from './examples/GetSetClear';
+import MergeItem from './examples/MergeItem';
 
-import AsyncStorage from '@react-native-async-storage/async-storage';
+const SCREENS = {
+  Functional,
+  GetSetClear,
+  MergeItem,
+  Basic,
+};
 
-function App() {
-  const [key, setKey] = React.useState<string>('');
-  const [value, setValue] = React.useState<string>('');
+export default function App(): JSX.Element {
+  const [iteration, setIteration] = useState(0);
+  const [currentTest, setCurrentTest] = useState(SCREENS.Functional);
+
+  const dismissKeyboard = useCallback(Keyboard.dismiss, []);
+  const simulateRestart = useCallback(
+    () => setIteration(iteration + 1),
+    [iteration],
+  );
+  const navigationBar = useMemo(
+    () =>
+      Object.values(SCREENS).map(t => {
+        const {testId, title} = t;
+        return (
+          <Button
+            key={testId}
+            testID={testId}
+            title={title}
+            onPress={() => setCurrentTest(t)}
+          />
+        );
+      }),
+    [],
+  );
 
   return (
-    <View style={{flex: 1, justifyContent: 'center'}}>
-      <TextInput
-        placeholder="Key"
-        value={key}
-        onChangeText={setKey}
-        style={{
-          borderWidth: 1,
-          borderBottomColor: '#aaa',
-          margin: 10,
-          paddingVertical: 10,
-        }}
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity
+        style={styles.closeKeyboardView}
+        onPress={dismissKeyboard}
+        testID="closeKeyboard"
       />
-      <TextInput
-        placeholder="Value"
-        value={value}
-        onChangeText={setValue}
-        style={{
-          borderWidth: 1,
-          borderBottomColor: '#aaa',
-          margin: 10,
-          paddingVertical: 10,
-        }}
-      />
-      <Button
-        title="Save"
-        onPress={() => {
-          AsyncStorage.setItem(key, value);
-        }}
-      />
-      <Button
-        title="Load"
-        onPress={() => {
-          AsyncStorage.getItem(key, (err, result) => {
-            if (result) {
-              setValue(result);
-            }
-          });
-        }}
-      />
-    </View>
+      <TouchableOpacity
+        testID="restart_button"
+        onPress={simulateRestart}
+        style={styles.restartButton}
+        activeOpacity={0.6}>
+        <Text>Simulate Restart</Text>
+      </TouchableOpacity>
+
+      <View style={styles.testPickerContainer}>{navigationBar}</View>
+
+      <View
+        testID={`${currentTest.testId}-view`}
+        key={currentTest.title + iteration}
+        style={styles.exampleContainer}>
+        <Text style={styles.exampleTitle}>{currentTest.title}</Text>
+        <Text style={styles.exampleDescription}>{currentTest.description}</Text>
+        <View style={styles.exampleInnerContainer}>{currentTest.render()}</View>
+      </View>
+    </SafeAreaView>
   );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+    padding: 8,
+  },
+  exampleContainer: {
+    padding: 4,
+    backgroundColor: '#FFF',
+    borderColor: '#EEE',
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    flex: 1,
+  },
+  exampleTitle: {
+    fontSize: 18,
+  },
+  exampleDescription: {
+    color: '#333333',
+    marginBottom: 16,
+  },
+  exampleInnerContainer: {
+    borderColor: '#EEE',
+    borderTopWidth: 1,
+    paddingTop: 10,
+    flex: 1,
+  },
+  restartButton: {
+    padding: 6,
+    borderRadius: 5,
+    backgroundColor: '#F3F3F3',
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'flex-end',
+  },
+  closeKeyboardView: {
+    width: 5,
+    height: 5,
+  },
+  testPickerContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+});
